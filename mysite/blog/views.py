cea53356f9e7878083e9f8b28a29c6898f1b6b48
basistus <basistus@i.ua>
2025-01-12 11:44:34 +0200

    Відправка постів по ел.пошті

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,81 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.core.paginator import Paginator, EmptyPage, \
+                                  PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
-# Create your views here.
+
+class PostListView(ListView):
+    """
+    Альтернативне подання списку постів
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
+
+
+def post_list(request):
+    posts_qs = Post.published.all()
+    # Посторінкова розбивка по 3 пости на сторінку
+    paginator = Paginator(posts_qs, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        # Якщо page_number не ціле число, то
+        # видати першу сторіку
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Якщо page_number знаходиться поза діапазоном, то
+        # видати останню сорінку
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts})
+
+
+def post_share(request, post_id):
+    # Дістати пост по ідентифікатору id
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == 'POST':
+        # Форма була передана на обробку
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Поля форми успішно пройшли валідацію
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']} рекомендовано Вам для прочитання " \
+                      f"{post.title}"
+            message = f"Прочитайте {post.title} за посиланням {post_url}\n\n" \
+                      f"{cd['name']}\'s коментарі: {cd['comments']}"
+            send_mail(subject, message, 'your_account@gmail.com',
+                      [cd['to']])
+            sent = True
+            # ... відправити електронний лист
+    else:
+        form = EmailPostForm()
+    return render(request,
+                  'blog/post/share.html',
+                  {'post': post, 'form': form, 'sent': sent})
